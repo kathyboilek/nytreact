b71afb2ad8abbc747272a4965aff58197618828b
Splumber <sajjadplumber@sajjads-mbp.attlocal.net>
2018-10-04 01:28:58 -0400

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Button from '@material-ui/core/Button';
-// import ButtonAppBar from '@material-ui/core/SnackbarContent';
-// import Appbar from "./components/Appbar/ButtonAppBar";
-import Appbar from "./components/MenuItem/MenuItem";
-import Input from "./components/Form/Input";
 import Nav from "./components/Nav";
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import Search from './pages/Search';
